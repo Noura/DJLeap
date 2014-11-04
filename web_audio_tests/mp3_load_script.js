f1392515e7f9c0853cdccb5a906b9c76f85e2398
Noura <culurienneldoreth@gmail.com>
2014-11-03 20:50:04 -0800

    volume of song changes with y position of hand

--- web_audio_tests/mp3_load_script.js
+++ web_audio_tests/mp3_load_script.js
@@ -1,51 +1,88 @@
 // Create the audio context
-var context = init();
+window.onload = function() {
 
-function init() {
-  try {
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    context = new AudioContext();
-  }
-  catch(e) {
-    alert('Web Audio API is not supported in this browser');
-  }
-  
-  alert('Context created');
-  return context;
-}
+    console.log('javascript file started');
+    var context = init();
 
-// Code to load the MP3
-var mp3Buffer = null;
+    var bufferSize = 4096;
 
-function loadMP3(url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
+    var hand = {
+        pinch: 0,
+        x: 0,
+        y: 0,
+    };
+    Leap.loop({ enableGestures: true }, function(frame) {
+        // left and right hands
+        for (var i = 0; i < frame.hands.length; i++) {
+            var h = frame.hands[i];
+            hand.pinch = h.pinchStrength;
+            hand.x = h.palmPosition[0];
+            hand.y = h.palmPosition[1];
+        }
+        console.log('hand x', hand.x, 'y', hand.y, 'pinch', hand.pinch);
+    });
 
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response,
-        function(buffer) {
-            mp3Buffer = buffer;
-            playSound(mp3Buffer);
-        },
-        function(e) {alert("Error with decoding audio data" + e.err)}
-    );
-  }
-  request.send();
-  
-  alert('MP3 request sent');
-}
+    function init() {
+      try {
+        // Fix up for prefixing
+        window.AudioContext = window.AudioContext||window.webkitAudioContext;
+        context = new AudioContext();
+      }
+      catch(e) {
+        console.log('Web Audio API is not supported in this browser');
+      }
+      
+      console.log('Context created');
+      return context;
+    }
 
-// Code to play the MP3
-function playSound(buffer) {
-  alert('About to start playing');
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
-}
+    // Code to load the MP3
+    var mp3Buffer = null;
 
-// Now actually load and play the MP3
-loadMP3('tidal.mp3');
+    function loadMP3(url) {
+      console.log('loadMP3');
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.responseType = 'arraybuffer';
+
+      // Decode asynchronously
+      request.onload = function() {
+        context.decodeAudioData(request.response,
+            function(buffer) {
+                mp3Buffer = buffer;
+                playSound(mp3Buffer);
+            },
+            function(e) {console.log("Error with decoding audio data" + e.err);}
+        );
+      }
+      request.send();
+      
+      console.log('MP3 request sent');
+    }
+
+    // Code to play the MP3
+    function playSound(buffer) {
+      console.log('About to start playing');
+      var source = context.createBufferSource(); // creates a sound source
+      source.buffer = buffer;                    // tell the source which sound to play
+      source.connect(empty_effect);
+      empty_effect.connect(context.destination);
+      source.start(0);                           // play the source now
+    }
+
+    var empty_effect = (function() {
+        var node = context.createScriptProcessor(bufferSize, 1, 1);
+        node.onaudioprocess = function(e) {
+            var input = e.inputBuffer.getChannelData(0);
+            var output = e.outputBuffer.getChannelData(0);
+            for (var i = 0; i < bufferSize; i++) {
+                var vol = Math.pow(1.01, hand.y)/150.0;
+                output[i] = input[i] * vol;
+            }
+        };
+        return node;
+    })();
+
+    // Now actually load and play the MP3
+    loadMP3('robyn_call_your_girlfriend.m4a');
+};
