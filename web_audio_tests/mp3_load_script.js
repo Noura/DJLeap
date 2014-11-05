35470a4eb46e10175285c9d0212f58635a51e2ee
Noura <culurienneldoreth@gmail.com>
2014-11-05 09:25:42 -0800

    working now it seems

--- web_audio_tests/mp3_load_script.js
+++ web_audio_tests/mp3_load_script.js
@@ -1,5 +1,33 @@
 // Create the audio context
 var context = init();
+var buffers = {
+    song: null,
+    reverb: null,
+};
+var bufferSize = 4096;
+var gain1 = context.createGain();
+gain1.gain.value = 0.5;
+var gain2 = context.createGain();
+gain2.gain.value = 0.5;
+
+ var hand = {
+     pinch: 0,
+     x: 0,
+     y: 0,
+ };
+Leap.loop({ enableGestures: true }, function(frame) {
+    // left and right hands
+    for (var i = 0; i < frame.hands.length; i++) {
+        var h = frame.hands[i];
+        hand.pinch = h.pinchStrength;
+        hand.x = h.palmPosition[0];
+        hand.y = h.palmPosition[1];
+    }
+    gain2.gain.value = Math.abs(hand.x)/300.0;
+    gain1.gain.value = 1.0 - gain2.gain.value;
+
+    console.log('hand x', hand.x, 'y', hand.y, 'pinch', hand.pinch);
+});
 
 function init() {
     try {
@@ -15,38 +43,69 @@ function init() {
     return context;
 }
 
-// Code to load the MP3
-var mp3Buffer = null;
-
-function loadMP3(url) {
+function loadMP3(url, buffer_key) {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.responseType = 'arraybuffer';
 
     // Decode asynchronously
-    request.onload = function() {
-        context.decodeAudioData(request.response,
-                function(buffer) {
-                    mp3Buffer = buffer;
-                    playSound(mp3Buffer);
-                },
-                function(e) {
-                    console.log("Error with decoding audio data" + e.err)}
-                );
-    }
+   request.onload = function() {
+       console.log('request.onload');
+       context.decodeAudioData(request.response,
+               function(buffer) {
+                   buffers[buffer_key] = buffer;
+                   var bs = Object.keys(buffers);
+                   var play = true;
+                   for (var i = 0; i < bs.length; i++) {
+                       if (buffers[bs[i]] === null) {
+                           play = false;
+                           break;
+                       }
+                   }
+                   if (play) {
+                       playSound();
+                   }
+               },
+               function(e) {console.log("Error with decoding audio data" + e.err);}
+               );
+   };
     request.send();
 
-    console.log('MP3 request sent');
+    console.log('audio request sent');
 }
 
 // Code to play the MP3
-function playSound(buffer) {
+function playSound() {
     console.log('About to start playing');
-    var source = context.createBufferSource(); // creates a sound source
-    source.buffer = buffer;                    // tell the source which sound to play
-    source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-    source.start(0);                           // play the source now
+    var source = context.createBufferSource();
+    source.buffer = buffers.song;
+
+    var reverb = context.createConvolver();
+    reverb.buffer = buffers.reverb;
+
+    source.connect(gain1);
+    source.connect(reverb);
+    reverb.connect(gain2);
+    gain1.connect(volume_effect);
+    gain2.connect(volume_effect);
+    volume_effect.connect(context.destination);
+
+    source.start(0);
 }
 
+var volume_effect = (function() {
+    var node = context.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            var vol = Math.pow(1.01, hand.y - 50.0)/150.0;
+            output[i] = input[i] * vol;
+        }
+    };
+    return node;
+})();
+
 // Now actually load and play the MP3
-loadMP3('robyn_call_your_girlfriend.m4a');
+loadMP3('robyn_call_your_girlfriend.m4a', 'song');
+loadMP3('irHall.ogg', 'reverb');
